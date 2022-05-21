346099372135c8c75f35926782143dfe5f616c6d
Alec Glascock <alecglas@gmail.com>
2022-05-20 20:32:24 -0500

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 import Qwixx from './Qwixx/Qwixx';
 import Navbar from './Navbar';
