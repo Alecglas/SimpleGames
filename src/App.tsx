ac4c02406f007b8d1bef9bee384c6c5908b87ef7
Alec Glascock <alecglas@gmail.com>
2022-05-20 20:33:40 -0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage';
 import Qwixx from './Qwixx/Qwixx';
 import Navbar from './Navbar';
@@ -8,7 +8,7 @@ import './App.scss';
 function App() {
   return (
   <div>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename="/SimpleGames">
       <Navbar/>
       <div className="App">
         <Routes>
